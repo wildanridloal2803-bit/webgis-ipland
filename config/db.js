65b4818d3a13db7013b3fb0a14342017afb37eb0
wildanridloal2803-bit <wildanridloal2803@gmail.com>
2026-06-18 11:07:39 +0700

    perbaikan koneksi database

--- config/db.js
+++ config/db.js
@@ -1,15 +1,40 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
-// Membuat pool koneksi untuk efisiensi resource
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  connectTimeout: 20000,
+
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, '../ca.pem')).toString(),
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = pool;
+
+
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+
+// // Membuat pool koneksi untuk efisiensi resource
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+// module.exports = pool;
